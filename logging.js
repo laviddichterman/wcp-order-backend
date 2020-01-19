d98a93f1b32ee9e4cb94ac4cd39a25960b1d1540
Dave Lichterman <dave@windycitypie.com>
2020-01-19 02:50:24 -0800

    Proper initialization of empty DB, part 1

---
+++ logging.js
@@ -0,0 +1,31 @@
+const winston = require('winston');
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss'
+    }),
+    winston.format.errors({ stack: true }),
+    winston.format.splat(),
+    winston.format.json()
+  ),
+  defaultMeta: { service: "wcp-order-backend" },
+  transports: [
+    //
+    // - Write to all logs with level `info` and below to `quick-start-combined.log`.
+    // - Write all logs error (and below) to `quick-start-error.log`.
+    //
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+      ),
+      level: 'debug'
+    })
+  ]
+});
+
+module.exports = logger;
--- server.js
+++ server.js
@@ -5,28 +5,27 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const WDateUtils = require("@wcp/wcpshared");
+const logger = require("./logging");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 const PORT = 4001;
-const DEFAULT_LEAD_TIME = 35;
 
 let LeadTimeSchema = require('./models/lead_time.model');
 let BlockedOffSchema = require('./models/blocked_off.model');
 let SettingsSchema = require('./models/settings.model');
 let StringListSchema = require('./models/string_list.model');
 
-
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/wcp',
-  { useNewUrlParser: true, useUnifiedTopology:true });
+mongoose.connect('mongodb://127.0.0.1:27017/wcp_05',
+  { useNewUrlParser: true, useUnifiedTopology: true });
 const connection = mongoose.connection;
 
-connection.once('open', function() {
-    console.log("MongoDB database connection established successfully");
+connection.once('open', function () {
+  logger.info("MongoDB database connection established successfully");
 })
 
 let WCP_SERVICES;
@@ -34,115 +33,171 @@ let WCP_LEAD_TIMES;
 let WCP_SETTINGS;
 let WCP_BLOCKED_OFF;
 
-
+// check if the database is bootstrapped and if not, load with defaults
 function BootstrapDatabase() {
-  //TODO: check if the database is bootstrapped and if not, load with defaults
-  StringListSchema.findOne(function(err, doc) {
-    //TODO check for errors
-    WCP_SERVICES = doc.services;
+  logger.info("Loading from and bootstrapping to database");
+  const DEFAULT_LEAD_TIMES = require("./data/leadtimeschemas.default.json");
+  const DEFAULT_SETTINGS = require("./data/settingsschemas.default.json");
+  const DEFAULT_SERVICES = require("./data/servicesschemas.default.json");
+  console.log(DEFAULT_SERVICES);
+  // look for services
+  StringListSchema.findOne(function (err, doc) {
+    if (err || !doc || !doc.services.length) {
+      WCP_SERVICES = DEFAULT_SERVICES;
+      let services_document = new StringListSchema(DEFAULT_SERVICES);
+      services_document.save()
+        .then(x => { logger.info("Added default services list: %o", services_document) })
+        .catch(err => { logger.error("Error adding default services list to database.", err); });
+    }
+    else {
+      logger.debug("Found services in database: ", doc.services);
+      WCP_SERVICES = doc.services;
+    }
+
+    // check for and populate lead times
     WCP_LEAD_TIMES = Array(WCP_SERVICES.length).fill(null);
-    LeadTimeSchema.find(function(err, leadtimes) {
-      console.log(leadtimes);
-      for (var i in leadtimes) {
-        WCP_LEAD_TIMES[leadtimes[i].service] = leadtimes[i].lead;
+    LeadTimeSchema.find(function (err, leadtimes) {
+      if (err || !leadtimes || !leadtimes.length) {
+        logger.info("Intializing LeadTimes with defaults.");
+        for (var i in DEFAULT_LEAD_TIMES) {
+          WCP_LEAD_TIMES[DEFAULT_LEAD_TIMES[i].service] = DEFAULT_LEAD_TIMES[i].lead;
+          let lt = new LeadTimeSchema({ service: i, lead: DEFAULT_LEAD_TIMES[i].lead });
+          lt.save()
+            .then(x => { logger.debug("Saved lead time of %o", lt) })
+            .catch(err => { logger.error("Error saving lead time %o", err); });
+        }
+      }
+      else {
+        for (var i in leadtimes) {
+          WCP_LEAD_TIMES[leadtimes[i].service] = leadtimes[i].lead;
+        }
+      }
+      if (leadtimes.length != WCP_SERVICES.length) { 
+        logger.error("we have a mismatch in service length and leadtimes stored in the DB");
       }
 
       //see if any leadtimes don't have a value yet and populate them
+      // this is being extra safe, we shouldn't get here.
       for (var j in WCP_LEAD_TIMES) {
         if (!WCP_LEAD_TIMES[j]) {
-          WCP_LEAD_TIMES[j] = DEFAULT_LEAD_TIME;
-          let lt = new LeadTimeSchema({service: j, lead: DEFAULT_LEAD_TIME});
+          WCP_LEAD_TIMES[j] = 35;
+          let lt = new LeadTimeSchema({ service: j, lead: 35 });
+          logger.error("Missing leadtime value! %o", lt);
           lt.save()
-            .then(x => {})
-            .catch(err => {console.log("error adding default leadtime");});
+            .then(x => { logger.debug("Saved leadtime: %o", lt) })
+            .catch(err => { logger.error("Error saving lead time for missing value %o", err) });
         }
       }
       return leadtimes;
-    }).then(x => {});
+    }).then(x => { });
 
-    // pull blocked off
-    WCP_BLOCKED_OFF = Array(WCP_SERVICES.length).fill([]);
-    //BlockedOffSchema.deleteMany({}, function(err) {});
-    BlockedOffSchema.find(function(err, docs) {
-      //do this once
-      // const start = new Date();
-      // start.setHours(5);
-      // start.setMinutes(15);
-      // const end = new Date();
-      // end.setHours(6);
-      // end.setMinutes(15);
-      // const blocked = new BlockedOffSchema({service: 0, exclusion_date: new Date(), start: start, end:end});
-      // blocked.save().then(x => {});
-      console.log(docs);
-      for (var i in docs) {
-        const interval = [docs[i].start, docs[i].end];
-        WDateUtils.AddIntervalToService(docs[i].service,
-          docs[i].exclusion_date,
-          interval,
-          WCP_BLOCKED_OFF);
-        console.log(WCP_BLOCKED_OFF[0]);
+    // check for and populate settings, including operating hours
+    SettingsSchema.findOne(function (err, settings) {
+      if (err || !settings) {
+        logger.info("No settings found, populating from defaults: %o", DEFAULT_SETTINGS);
+        WCP_SETTINGS = DEFAULT_SETTINGS;
+        let settings_document = new SettingsSchema(DEFAULT_SETTINGS);
+        settings_document.save()
+          .then(x => { logger.debug("Saved settings: %o", settings_document) })
+          .catch(err => { logger.error("Error saving settings %o", err) });
+      }
+      else {
+        logger.info("Found settings: %o", settings);
+        console.log(settings);
+        WCP_SETTINGS = settings;
       }
     });
-    SettingsSchema.findOne(function(err, settings) {
-      console.log(settings);
-      WCP_SETTINGS = settings;
-    });
 
+    // populate blocked off array
+    WCP_BLOCKED_OFF = Array(WCP_SERVICES.length).fill([]);
+    BlockedOffSchema.find(function (err, blocked) {
+      if (err || !blocked || !blocked.length) {
+        logger.debug("No blocked off entries found. Creating blocked off array of length %o", WCP_SERVICES.length);
+        WCP_BLOCKED_OFF = Array(WCP_SERVICES.length).fill([]);
+        const blocked_off = new BlockedOffSchema({blocked_off: []});
+        blocked_off.save()
+          .then(e => { logger.debug("Saved blocked off %o", blocked_off) })
+          .catch(err => { logger.error("Error saving blocked off %o", err) });
+      }
+      else {
+        for (var i in blocked) {
+          const interval = [blocked[i].start, blocked[i].end];
+          logger.debug("Added blocked off: %o", blocked[i]);
+          WDateUtils.AddIntervalToService(blocked[i].service,
+            blocked[i].exclusion_date,
+            interval,
+            WCP_BLOCKED_OFF);
+        }
+      }
+    });
   });
 }
 
 
 BootstrapDatabase();
 
-io.on('connection', function(socket) {
-  //let services = new StringListSchema({services: ["Pick-up", "Dine-In", "Delivery"]});
-  //services.save().then(services => { console.log("saved services!")}).catch(err=>{console.log("bad news")});
-  console.log("new client connected");
+io.on('connection', function (socket) {
+  logger.info("New client connected.");
 
   socket.emit('WCP_SERVICES', WCP_SERVICES);
   socket.emit('WCP_LEAD_TIMES', WCP_LEAD_TIMES);
   socket.emit('WCP_BLOCKED_OFF', WCP_BLOCKED_OFF);
   socket.emit('WCP_SETTINGS', WCP_SETTINGS);
-  socket.on('WCP_SERVICES', function(msg) {
-    console.log("services message");
-    console.log(msg);
+  socket.on('WCP_SERVICES', function (msg) {
+    logger.debug("Got socket message on WCP_SERVICES channel: %o", msg);
     socket.broadcast.emit('WCP_SERVICES', WCP_SERVICES);
   });
-  socket.on('WCP_BLOCKED_OFF', function(msg) {
-    console.log("BLOCKED OFF MESSAGE");
-    console.log(msg);
+  socket.on('WCP_BLOCKED_OFF', function (msg) {
+    logger.debug("Got socket message on WCP_BLOCKED_OFF channel: %o", msg);
     WCP_BLOCKED_OFF = msg;
-    socket.broadcast.emit('WCP_BLOCKED_OFF', WCP_BLOCKED_OFF);
+    socket.broadcast.emit('WCP_BLOCKED_OFF', WCP_BLOCKED_OFF); 
+    let blocked_off = [];
+    for (var i in WCP_BLOCKED_OFF) {
+      for (var j in WCP_BLOCKED_OFF[i]) {
+        let excluded_intervals = [];
+        for (var k in WCP_BLOCKED_OFF[i][j][1]) {
+          excluded_intervals.push({start: WCP_BLOCKED_OFF[i][j][1][k][0], end: WCP_BLOCKED_OFF[i][j][1][k][1]})
+        }
+        blocked_off.push({service: i, exclusion_date: WCP_BLOCKED_OFF[i][j][0], excluded_intervals: excluded_intervals});
+      }
+    }
+    logger.debug("Generated blocked off array: %o", blocked_off);
+    BlockedOffSchema.replaceOne(function (err, blocked) {
+
+    });
+
   });
-  socket.on('WCP_LEAD_TIMES', function(msg) {
+  socket.on('WCP_LEAD_TIMES', function (msg) {
+    logger.debug("Got socket message on WCP_LEAD_TIMES channel: %o", msg);
     WCP_LEAD_TIMES = msg;
     socket.broadcast.emit('WCP_LEAD_TIMES', WCP_LEAD_TIMES);
-    LeadTimeSchema.find(function(err, leadtimes) {
+    LeadTimeSchema.find(function (err, leadtimes) {
       for (var i in leadtimes) {
         leadtimes[i].lead = WCP_LEAD_TIMES[leadtimes[i].service];
-        leadtimes[i].save().then(e => {});
+        leadtimes[i].save()
+          .then(x => { logger.debug("Saved leadtime: %o", leadtimes[i]) })
+          .catch(err => { logger.error("Error saving lead time %o", err); });
       }
       return leadtimes;
     });
   });
-  socket.on('WCP_SETTINGS', function(msg) {
+  socket.on('WCP_SETTINGS', function (msg) {
+    logger.debug("Got socket message on WCP_SETTINGS channel: %o", msg);
     WCP_SETTINGS = msg;
-    console.log("SETTING MESSAGE");
-    console.log(msg);
-
-    SettingsSchema.findOne(function(err, db_settings) {
+    socket.broadcast.emit('WCP_SETTINGS', WCP_SETTINGS);
+    SettingsSchema.findOne(function (err, db_settings) {
       Object.assign(db_settings, WCP_SETTINGS);
       db_settings.save()
-        .then(e => {})
-        .catch(err => {console.log("error updating settings");});
+        .then(e => { logger.debug("Saved settings %o", db_settings) })
+        .catch(err => { logger.error("Error saving settings %o", err) });
     });
-    socket.broadcast.emit('WCP_SETTINGS', WCP_SETTINGS);
   });
 });
 
 
-server.listen(PORT, function() {
-    console.log("Server is running on Port: " + PORT);
+server.listen(PORT, function () {
+  logger.debug("%o", server);
+  logger.info("Server is running on Port: " + PORT);
 });
 
 module.exports = server;
