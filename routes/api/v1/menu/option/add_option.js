addc14004a2452e0734a45f50c297ef31b942c98
Dave Lichterman <dave@windycitypie.com>
2020-06-13 16:13:30 -0700

    otid to mtid

--- routes/api/v1/menu/option/add_option.js
+++ routes/api/v1/menu/option/add_option.js
@@ -4,7 +4,7 @@ const { body, param, validationResult } = require('express-validator');
 const { CheckJWT } = require('../../../../../config/authorization');
 
 const ValidationChain = [  
-  param('otid').trim().escape().exists(),
+  param('mtid').trim().escape().exists(),
   body('display_name').trim().escape().exists(),
   body('description').trim().escape(),
   body('shortcode').trim().escape().exists(),
@@ -22,18 +22,18 @@ const ValidationChain = [
 ];
 
 module.exports = Router({ mergeParams: true })
-  .post('/v1/menu/option/:otid/', ValidationChain, CheckJWT, async (req, res, next) => {
+  .post('/v1/menu/option/:mtid/', ValidationChain, CheckJWT, async (req, res, next) => {
     try {
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
         return res.status(422).json({ errors: errors.array() });
       }
       req.db.WOptionTypeSchema.findById(
-        req.params.otid,
+        req.params.mtid,
         async (err, doc) => {
           if (err) {
-            req.logger.info(`Unable to find option type to add option to: ${req.params.otid}`);
-            return res.status(404).send(`Unable to find option type to add option to: ${req.params.otid}`);;
+            req.logger.info(`Unable to find option type to add option to: ${req.params.mtid}`);
+            return res.status(404).send(`Unable to find option type to add option to: ${req.params.mtid}`);;
           }
           else {
             const newoption = await req.catalog.CreateOption({
@@ -44,7 +44,7 @@ module.exports = Router({ mergeParams: true })
               disabled: req.body.disabled,
               revelID: req.body.revelID,
               squareID: req.body.squareID,
-              option_type_id: req.params.otid,
+              option_type_id: req.params.mtid,
               ordinal: req.body.ordinal,
               flavor_factor: req.body.flavor_factor || 0,
               bake_factor: req.body.bake_factor || 0,
