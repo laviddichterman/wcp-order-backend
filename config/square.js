d09e3966e7f1266428dc024385ee962c457113bf
Dave Lichterman <dave@windycitypie.com>
2021-05-04 01:00:23 -0700

    wow... missed the bulk of the changes

--- config/square.js
+++ config/square.js
@@ -1,8 +1,8 @@
-const SquareConnect = require('square-connect');
-const {  ApiError, Client, Environment, CreatePaymentRequest, Money } = require('square')
+const {  ApiError, Client, Environment } = require('square')
 
 const crypto = require('crypto');
 const logger = require('../logging');
+const { http } = require('../logging');
 
 class SquareProvider {
   #client;
@@ -14,7 +14,6 @@ class SquareProvider {
     const cfg = db.KeyValueConfig;
     if (cfg.SQUARE_TOKEN && cfg.SQUARE_LOCATION) {
       this.#client = new Client({
-        timeout: 3000,
         environment: Environment.Production, // `Environment.Sandbox` to access sandbox resources // TODO: configure this
         accessToken: cfg.SQUARE_TOKEN,
       });
@@ -25,29 +24,30 @@ class SquareProvider {
     }
   }
 
-  CreateOrderStoreCredit = async (reference_id, amount_money) => {
+  CreateOrderStoreCredit = async (reference_id, amount_money, note) => {
     const idempotency_key = crypto.randomBytes(22).toString('hex');
     const orders_api = this.#client.ordersApi;
     const request_body = {
-      idempotency_key: idempotency_key,
+      idempotencyKey: idempotency_key,
       order: {
-        reference_id: reference_id,
-        line_items: [{
+        referenceId: reference_id,
+        lineItems: [{
           quantity: "1",
-          catalog_object_id: "DNP5YT6QDIWTB53H46F3ECIN",
-          base_price_money: {
+          catalogObjectId: "DNP5YT6QDIWTB53H46F3ECIN",
+          basePriceMoney: {
             "amount": amount_money,
             "currency": "USD"
-          }
+          },
+          note: note
         }],
-        location_id: this.#location_id,
+        locationId: this.#location_id,
         state: "OPEN",
       }
     };
     try {
       logger.info(`sending order request: ${JSON.stringify(request_body)}`);
-      const response = await orders_api.createOrder(this.#location_id, request_body);
-      return { success: true, response: response };
+      const { result, ...httpResponse } = await orders_api.createOrder(request_body);
+      return { success: true, response: result };
     } catch (error) {
       logger.error(error);
       return {
@@ -61,17 +61,17 @@ class SquareProvider {
     const idempotency_key = crypto.randomBytes(22).toString('hex');
     const orders_api = this.#client.ordersApi;
     const request_body = {
-      idempotency_key: idempotency_key,
+      idempotencyKey: idempotency_key,
       order: {
-        location_id: this.#location_id,
+        locationId: this.#location_id,
         version: order_version,
         state: new_state,
       }
     };
     try {
       logger.info(`sending order status change request: ${JSON.stringify(request_body)}`);
-      const response = await orders_api.updateOrder(this.#location_id, square_order_id, request_body);
-      return { success: true, response: response };
+      const { result, ...httpResponse } = await orders_api.updateOrder(square_order_id, request_body);
+      return { success: true, response: result };
     } catch (error) {
       logger.error(error);
       return {
@@ -83,7 +83,7 @@ class SquareProvider {
 
   ProcessPayment = async (nonce, amount_money, reference_id, square_order_id) => {
     const idempotency_key = crypto.randomBytes(22).toString('hex');
-    const payments_api = new SquareConnect.PaymentsApi();
+    const payments_api = this.#client.paymentsApi;
     const request_body = {
       sourceId: nonce,
       amountMoney: {
@@ -101,13 +101,13 @@ class SquareProvider {
     };
     try {
       logger.info(`sending payment request: ${JSON.stringify(request_body)}`);
-      const response = await payments_api.createPayment(request_body);
-      return { success: true, result: response };
+      const { result, ...httpResponse } = await payments_api.createPayment(request_body);
+      return { success: true, result: result };
     } catch (error) {
       logger.error(error);
       return {
         success: false,
-        result: error.response.text
+        result: error.result
       };
     }
   }
