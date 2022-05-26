d6bf12e40f85da7e4b469caf140088d7a2ada472
Dave Lichterman <me@lavid.me>
2022-05-26 08:48:11 -0700

    ENV var handling changes
    * use dotenv package
    * require and error on missing AUTH_DOMAIN
    * require and error on missing AUTH_AUDIENCE

--- config/authorization.js
+++ config/authorization.js
@@ -1,10 +1,19 @@
 const { auth, requiredScopes } = require('express-oauth2-jwt-bearer');
+const logger = require('../logging');
+
+if (!process.env.AUTH_DOMAIN) { 
+  logger.error("Missing config for AUTH_DOMAIN ");
+  process.exit(1);
+}
+if (!process.env.AUTH_AUDIENCE) { 
+  logger.error("Missing config for AUTH_AUDIENCE");
+  process.exit(1);
+}
 
 const authConfig = {
-  domain: "lavid.auth0.com",
-  audience: process.env.AUTH_AUDIENCE || "https://wario.windycitypie.com"
+  domain: process.env.AUTH_DOMAIN,
+  audience: process.env.AUTH_AUDIENCE
 };
-
 const CheckJWT = auth({
   audience: authConfig.audience,
   issuerBaseURL: `https://${authConfig.domain}/`,
