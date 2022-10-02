31386443ce83f67b51cd37e5fa21a8e3b837f56c
Dave Lichterman <me@lavid.me>
2022-10-02 16:10:21 -0700

    v 0.5.30 new square catalog integration

--- src/config/database_manager.ts
+++ src/config/database_manager.ts
@@ -485,6 +485,8 @@ const UPGRADE_MIGRATION_FUNCTIONS: IMigrationFunctionObject = {
   "0.5.27": [{ major: 0, minor: 5, patch: 28 }, async () => {
   }],
   "0.5.28": [{ major: 0, minor: 5, patch: 29 }, async () => {
+  }],
+  "0.5.29": [{ major: 0, minor: 5, patch: 30 }, async () => {
     // any split options or OTS enabled, if the modifier type is single select, disable split and ots
   }],
 }
