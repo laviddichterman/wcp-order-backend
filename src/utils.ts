8cacc136d3efa0a55b63138faf5e31076aca7b5b
Dave Lichterman <me@lavid.me>
2022-10-11 18:34:49 -0700

    some WIP(stolen) code for exponential backoff

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,70 @@
+// import { RetryConfiguration } from "square/dist/core";
 import logger from "./logging";
 
 export const IS_PRODUCTION = process.env.NODE_ENV !== 'development';
+
+
+// /**
+//  * Returns wait time for the request
+//  * @param retryConfig Configuration for retry
+//  * @param method HttpMethod of the request
+//  * @param allowedWaitTime Remaining allowed wait time
+//  * @param retryCount Retry attempt number
+//  * @param httpCode Status code received
+//  * @param headers Response headers
+//  * @param timeoutError Error from the server
+//  * @returns Wait time before the retry
+//  */
+// function getRetryWaitTime(retryConfig: RetryConfiguration, allowedWaitTime: number, retryCount : number, httpCode :number, headers: Record<string, string>, timeoutError: boolean) {
+//     let retryWaitTime = 0.0;
+//     let retry = false;
+//     let retryAfter = 0;
+//     if (retryCount < retryConfig.maxNumberOfRetries) {
+//         if (timeoutError) {
+//             retry = retryConfig.retryOnTimeout;
+//         }
+//         else if (typeof headers !== 'undefined' &&
+//             typeof httpCode !== 'undefined') {
+//             retryAfter = getRetryAfterSeconds(headers);
+//             retry =
+//                 retryAfter > 0 || retryConfig.httpStatusCodesToRetry.includes(httpCode);
+//         }
+//         if (retry) {
+//             var noise = +(Math.random() / 100).toFixed(3);
+//             var waitTime = retryConfig.retryInterval *
+//                 Math.pow(retryConfig.backoffFactor, retryCount) +
+//                 noise;
+//             waitTime = Math.max(waitTime, retryAfter);
+//             if (waitTime <= allowedWaitTime) {
+//                 retryWaitTime = waitTime;
+//             }
+//         }
+//     }
+//     return retryWaitTime;
+// }
+// exports.getRetryWaitTime = getRetryWaitTime;
+// function getRetryAfterSeconds(headers: Record<string, string>) {
+//   const retryAfter = Object.hasOwn()
+//     if (retryAfter == null) {
+//         return 0;
+//     }
+//     if (isNaN(+retryAfter)) {
+//         var timeDifference = (new Date(retryAfter).getTime() - Date.now()) / 1000;
+//         return isNaN(timeDifference) ? 0 : timeDifference;
+//     }
+//     return +retryAfter;
+// }
+// function shouldRetryRequest(retryConfig, retryOption, httpMethod) {
+//     switch (retryOption) {
+//         case RequestRetryOption.Default:
+//             return retryConfig.httpMethodsToRetry.includes(httpMethod);
+//         case RequestRetryOption.Enable:
+//             return true;
+//         case RequestRetryOption.Disable:
+//             return false;
+//     }
+// }
+
 
 export async function ExponentialBackoff<T>(
   request : () => Promise<T>, 
